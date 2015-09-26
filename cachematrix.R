1d1306ea74671c0cc4c24e26136fa91f553884ac
Erkrath <juanlu_itatpc@hotmail.com>
2015-09-26 23:18:49 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,30 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+#####makeCacheMatrix: This function creates a special "matrix" (Saved) object that can cache its Inv_Matrix.
 
+makeCacheMatrix <- function(x,Inv_Matrix) {
+  Saved <- list(x,Inv_Matrix)
 }
 
 
-## Write a short comment describing this function
+#####cacheSolve:  This function computes the Inv_Matrix of the special "matrix" returned by makeCacheMatrix above. 
+#####             If the Inv_Matrix has already been calculated (and the matrix has not changed), 
+#####             then the cachesolve should retrieve the Inv_Matrix from the cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Write a short comment describing this function
+cacheSolve <- function(x) {
+  #Check if the Inv_Matrix has been calculated...
+  if(exists("Saved") == TRUE) {  #if so, retrieve data
+    message("getting Inv_Matrix from previous calculations")
+    Inv_Matrix <- Saved[2]
+  }
+  else { #if not, calculate the Inv_Matrix
+    Inv_Matrix <- solve(x)
+    message("Inv_Matrix has not been calculated previously. Calculating now...")
+    Saved <- makeCacheMatrix(x,Inv_Matrix)
+  }
+  # Return the Inv_Matrix
+  Inv_Matrix <- Saved[2]
+  print(Inv_Matrix)
 }
